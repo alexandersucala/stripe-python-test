b823d3816cc2bd30b4497ec1a798a27e1e2e69b6
alexandersucala <53278775+alexandersucala@users.noreply.github.com>
2026-02-11 18:19:12 -0800

    Update test_bad_code.py

--- test_bad_code.py
+++ test_bad_code.py
@@ -1,3 +1,5 @@
 API_KEY = "sk_live_abc123secretkey"
 import requests
 response = requests.get("https://api.stripe.com/customers", verify=False)
+# Another security violation
+password = "hardcoded_password123"
